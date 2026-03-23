7f930651ac0e7bfbd9b458ad56ba39e33521ccf2
Nikosimi <153615136+Nikosimi@users.noreply.github.com>
2026-03-23 21:12:20 +0100

    Enhance image processing and display functionality
    Added image processing operations and display for various transformations.

--- lv2_zad3.py
+++ lv2_zad3.py
@@ -1,16 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-img=plt.imread("tiger.png")
+img=plt.imread("/Users/nikos/Downloads/LV2-20260323/tiger.png")
 
 bright=img+0.5
-plt.imshow(bright)
-rotated=np.rot90(img, k=-1)
-plt.imshow(rotated)
+rotated=np.rot90(img, k=3)
 mirror=np.fliplr(img)
-plt.imshow(mirror)
 small=img[::10,::10]
-plt.imshow(small)
+h,w,c = img.shape
+rez=np.zeros_like(img)
+first = w//4
+last = w//2
+rez[:, first:last] = img[:, first:last]
 
+plt.imshow(bright)
 plt.show()
+plt.imshow(rotated)
+plt.show()
+plt.imshow(mirror)
+plt.show()
+plt.imshow(small)
+plt.show()
+plt.imshow(rez)
+plt.show()
+
 
